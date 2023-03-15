4612ea1e93f727a782be01ebf35923d8c62c21f3
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-03-15 12:44:26 +0300

    addPopup

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,12 @@ function Main(){
     return(
         <main className="main-content">
           <section className="profile section section_size_narrow">
-            <div className="profile__avatar-container">
+            <div className="profile__avatar-container"
+            onClick={() => {
+            const handleEditAvatarClick = document.querySelector(".popup_edit_avatar")
+            handleEditAvatarClick.classList.add('popup_opened');
+            }}
+            >
               <img
                 src="<%=require('./images/Avatar.png')%>"
                 alt="Аватар"
@@ -20,11 +25,18 @@ function Main(){
                   type="button"
                   aria-label="Редактировать профиль"
                   className="profile__edit"
+                onClick={() => {
+                const handleEditProfileClick = document.querySelector(".popup_edit-profile");
+                handleEditProfileClick.classList.add('popup_opened');
+                }}
                 />
               </div>
               <p className="profile__aboute">Исследователь океана</p>
             </div>
-            <button type="button" className="profile__add-button" />
+            <button type="button" className="profile__add-button" onClick={() => {
+              const handleAddPlaceClick = document.querySelector(".popup-cards");
+              handleAddPlaceClick.classList.add('popup_opened');
+            }}/>
           </section>
           <section className="elements" />
         </main>
