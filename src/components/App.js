47b5ef66d218c02f0a9a56088f9b87d051b3dc29
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-04-05 14:42:57 +0300

    pr11 start and work new IDE

--- src/components/App.js
+++ src/components/App.js
@@ -1,172 +1,206 @@
-import React from "react";
+import { useEffect, useState } from 'react';
 
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
-import PopupWithForm from "./PopupWithForm";
-import ImagePopup from "./ImagePopup";
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+import ImagePopup from './ImagePopup';
+import PopupWithForm from './PopupWithForm';
+
+
+import api from '../utils/Api';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
 
 function App() {
-  //использую хуки, чтобы задать начальное состояние(false)
-  const [isEditProfilePopupOpen, setEditProfilePopupOpen] =
-    React.useState(false);
-  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
-  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
-  const [isConfirmDeletePopup, setConfirmDeletePopup] = React.useState(false);
-  const [isCardOpen, setCardOpen] = React.useState(false);
-  const [isSelectedCard, setSelectedCard] = React.useState({});
+	//использую хуки, чтобы задать начальное состояние(false)
+	const [isEditProfilePopupOpen, setEditProfilePopupOpen] =
+		useState(false);
+	const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
+	const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
+	const [isConfirmDeletePopup, setConfirmDeletePopup] = useState(false);
+	const [isCardOpen, setCardOpen] = useState(false);
+	const [isSelectedCard, setSelectedCard] = useState({});
 
-  //описываю функции для всех изменений начального состояния
-  function handleEditProfileClick() {
-    setEditProfilePopupOpen(true);
-  }
+	// Создайте стейт currentUser в корневом компоненте
+	const [currentUser, setCurrentUser] = useState('');
 
-  function handleAddPlaceClick() {
-    setAddPlacePopupOpen(true);
-  }
 
-  function handleEditAvatarClick() {
-    setEditAvatarPopupOpen(true);
-  }
+	const [cards, setCard] = useState([]);
 
-  //функция закрытия всех попапов
-  function closeAllPopups() {
-    setCardOpen(false);
-    setEditProfilePopupOpen(false);
-    setAddPlacePopupOpen(false);
-    setEditAvatarPopupOpen(false);
-    setConfirmDeletePopup(false);
-  }
+	useEffect(() => {
+		Promise.all([api.getUserInfo(), api.getInitialCards()])
+			.then((data) => {
+				const [userData, cardsData] = data;
 
-  function handleConfirmDeletePopup() {
-    setConfirmDeletePopup(true);
-  }
+				setCurrentUser(userData)
 
-  function handleOpenCardClick(cardsData) {
-    setCardOpen(true);
-    setSelectedCard(cardsData);
-  }
+				setCard(cardsData)
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  return (
-    <>
-      <div className="main-page">
-        <Header />
-        <Main
-          openProfileEdit={handleEditProfileClick}
-          addButtonCard={handleAddPlaceClick}
-          openUserAvatar={handleEditAvatarClick}
-          openDeleteConfirm={handleConfirmDeletePopup}
-          openCard={handleOpenCardClick}
-        />
-        <Footer />
-        <PopupWithForm
-          name="avatar"
-          title="Обновить аватар"
-          buttonText="Сохранить"
-          isOpen={isEditAvatarPopupOpen}
-          isClosed={closeAllPopups}
-        >
-          <input
-            type="url"
-            name="avatar"
-            placeholder="Ссылка на картинку"
-            className="popup__input popup__input_avatar-link"
-            id="avatar-link"
-            required=""
-          />
-          <span className="avatar-link-error popup__input-error">
+
+	function handleCardLike(card) {
+		const isLiked = card.likes.some(i => i._id === currentUser._id);
+
+		api.
+	}
+
+
+	//описываю функции для всех изменений начального состояния
+	function handleEditProfileClick() {
+		setEditProfilePopupOpen(true);
+	}
+
+	function handleAddPlaceClick() {
+		setAddPlacePopupOpen(true);
+	}
+
+	function handleEditAvatarClick() {
+		setEditAvatarPopupOpen(true);
+	}
+
+	//функция закрытия всех попапов
+	function closeAllPopups() {
+		setCardOpen(false);
+		setEditProfilePopupOpen(false);
+		setAddPlacePopupOpen(false);
+		setEditAvatarPopupOpen(false);
+		setConfirmDeletePopup(false);
+	}
+
+	function handleConfirmDeletePopup() {
+		setConfirmDeletePopup(true);
+	}
+
+	function handleOpenCardClick(cardsData) {
+		setCardOpen(true);
+		setSelectedCard(cardsData);
+	}
+
+	return (
+		<>
+			<div className='main-page'>
+				<Header />
+				<Main
+					openProfileEdit={handleEditProfileClick}
+					addButtonCard={handleAddPlaceClick}
+					openUserAvatar={handleEditAvatarClick}
+					openDeleteConfirm={handleConfirmDeletePopup}
+					openCard={handleOpenCardClick}
+				/>
+				<Footer />
+				<PopupWithForm
+					name='avatar'
+					title='Обновить аватар'
+					buttonText='Сохранить'
+					isOpen={isEditAvatarPopupOpen}
+					isClosed={closeAllPopups}
+				>
+					<input
+						type='url'
+						name='avatar'
+						placeholder='Ссылка на картинку'
+						className='popup__input popup__input_avatar-link'
+						id='avatar-link'
+						required=''
+					/>
+					<span className='avatar-link-error popup__input-error'>
             вы пропустили поле.
           </span>
-        </PopupWithForm>
+				</PopupWithForm>
 
-        {/* для редактирования профиля */}
-        <PopupWithForm
-          name="user"
-          title="Редактировать профиль"
-          buttonText="Сохранить"
-          isOpen={isEditProfilePopupOpen}
-          isClosed={closeAllPopups}
-        >
-          <input
-            type="text"
-            id="input-name"
-            name="dataName"
-            className="popup__input"
-            placeholder="Как вас зовут?"
-            minLength={2}
-            maxLength={40}
-            required=""
-          />
-          <span className="input-name-error popup__input-error">
+				{/* для редактирования профиля */}
+				<PopupWithForm
+					name='user'
+					title='Редактировать профиль'
+					buttonText='Сохранить'
+					isOpen={isEditProfilePopupOpen}
+					isClosed={closeAllPopups}
+				>
+					<input
+						type='text'
+						id='input-name'
+						name='dataName'
+						className='popup__input'
+						placeholder='Как вас зовут?'
+						minLength={2}
+						maxLength={40}
+						required=''
+					/>
+					<span className='input-name-error popup__input-error'>
             вы пропустили поле.
           </span>
-          <input
-            type="text"
-            name="dataJob"
-            placeholder="Чем вы занимаетесь?"
-            className="popup__input"
-            id="input-job"
-            minLength={2}
-            maxLength={200}
-            required=""
-          />
-          <span className="input-job-error popup__input-error">
+					<input
+						type='text'
+						name='dataJob'
+						placeholder='Чем вы занимаетесь?'
+						className='popup__input'
+						id='input-job'
+						minLength={2}
+						maxLength={200}
+						required=''
+					/>
+					<span className='input-job-error popup__input-error'>
             вы пропустили поле.
           </span>
-        </PopupWithForm>
+				</PopupWithForm>
 
-        {/* для добавления карточек */}
-        <PopupWithForm
-          name="cardName"
-          title="Новое место"
-          buttonText="Создать"
-          isOpen={isAddPlacePopupOpen}
-          isClosed={closeAllPopups}
-        >
-          <input
-            type="text"
-            name="cardName"
-            placeholder="Название?"
-            className="popup__input"
-            id="card-name"
-            minLength={2}
-            maxLength={30}
-            required=""
-          />
-          <span className="card-name-error popup__input-error">
+				{/* для добавления карточек */}
+				<PopupWithForm
+					name='cardName'
+					title='Новое место'
+					buttonText='Создать'
+					isOpen={isAddPlacePopupOpen}
+					isClosed={closeAllPopups}
+				>
+					<input
+						type='text'
+						name='cardName'
+						placeholder='Название?'
+						className='popup__input'
+						id='card-name'
+						minLength={2}
+						maxLength={30}
+						required=''
+					/>
+					<span className='card-name-error popup__input-error'>
             вы пропустили поле.
           </span>
-          <input
-            type="url"
-            name="cardLink"
-            placeholder="Ссылка на картинку"
-            className="popup__input"
-            id="card-link"
-            required=""
-          />
-          <span className="card-link-error popup__input-error">
+					<input
+						type='url'
+						name='cardLink'
+						placeholder='Ссылка на картинку'
+						className='popup__input'
+						id='card-link'
+						required=''
+					/>
+					<span className='card-link-error popup__input-error'>
             вы пропустили поле.
           </span>
-        </PopupWithForm>
+				</PopupWithForm>
 
-        {/* для открытия картинки */}
-        <ImagePopup
-          card={isSelectedCard}
-          isOpen={isCardOpen}
-          isClosed={closeAllPopups}
-        ></ImagePopup>
+				{/* для открытия картинки */}
+				<ImagePopup
+					card={isSelectedCard}
+					isOpen={isCardOpen}
+					isClosed={closeAllPopups}
+				></ImagePopup>
 
-        {/* для подтверждения удаления  */}
-        <PopupWithForm
-          name="delete-card"
-          title="Вы уверены?"
-          buttonText="Да"
-          isOpen={isConfirmDeletePopup}
-          isClosed={closeAllPopups}
-        ></PopupWithForm>
-      </div>
-    </>
-  );
+				{/* для подтверждения удаления  */}
+				<PopupWithForm
+					name='delete-card'
+					title='Вы уверены?'
+					buttonText='Да'
+					isOpen={isConfirmDeletePopup}
+					isClosed={closeAllPopups}
+				></PopupWithForm>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,139 +1,139 @@
 class Api {
-  constructor({baseUrl, headers}) {
-    this._baseUrl = baseUrl;
-    this._headers = headers;
-  }
+	constructor({ baseUrl, headers }) {
+		this._baseUrl = baseUrl;
+		this._headers = headers;
+	}
 
-  getUserInfo() {
-    return fetch(`${this._baseUrl}users/me`, {
-      method: "GET",
-      headers: this._headers,
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	getUserInfo() {
+		return fetch(`${this._baseUrl}users/me`, {
+			method: 'GET',
+			headers: this._headers
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  getInitialCards() {
-    return fetch(`${this._baseUrl}cards`, {
-      method: "GET",
-      headers: this._headers,
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	getInitialCards() {
+		return fetch(`${this._baseUrl}cards`, {
+			method: 'GET',
+			headers: this._headers
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  updateUserAvatar(avatar) {
-    return fetch(`${this._baseUrl}users/me/avatar`, {
-      headers: this._headers,
-      method: "PATCH",
-      body: JSON.stringify(avatar),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	updateUserAvatar(avatar) {
+		return fetch(`${this._baseUrl}users/me/avatar`, {
+			headers: this._headers,
+			method: 'PATCH',
+			body: JSON.stringify(avatar)
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  updateUserInfo(userInfo) {
-    return fetch(`${this._baseUrl}users/me`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify(userInfo),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	updateUserInfo(userInfo) {
+		return fetch(`${this._baseUrl}users/me`, {
+			method: 'PATCH',
+			headers: this._headers,
+			body: JSON.stringify(userInfo)
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  createCard(cardInfo) {
-    return fetch(`${this._baseUrl}cards`, {
-      method: "POST",
-      headers: this._headers,
-      body: JSON.stringify(cardInfo),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	createCard(cardInfo) {
+		return fetch(`${this._baseUrl}cards`, {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify(cardInfo)
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  deleteCard(cardId) {
-    return fetch(`${this._baseUrl}cards/${cardId}`, {
-      method: "DELETE",
-      headers: this._headers,
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	deleteCard(cardId) {
+		return fetch(`${this._baseUrl}cards/${cardId}`, {
+			method: 'DELETE',
+			headers: this._headers
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  addLike(cardId) {
-    return fetch(`${this._baseUrl}cards/likes/${cardId}`, {
-      method: "PUT",
-      headers: this._headers,
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	addLike(cardId, isLiked) {
+		return fetch(`${this._baseUrl}cards/likes/${cardId}`, {
+			method: isLiked ? 'PUT' : 'DELETE',
+			headers: this._headers
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  deleteLike(cardId) {
-    return fetch(`${this._baseUrl}cards/likes/${cardId}`, {
-      method: "DELETE",
-      headers: this._headers,
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	deleteLike(cardId) {
+		return fetch(`${this._baseUrl}cards/likes/${cardId}`, {
+			method: 'DELETE',
+			headers: this._headers
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 }
 
 const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-60/",
-  headers: {
-    authorization: "8449ac58-7a24-4246-806a-a59752bbc1d5",
-    "Content-Type": "application/json",
-  }
-})
+	baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-60/',
+	headers: {
+		authorization: '8449ac58-7a24-4246-806a-a59752bbc1d5',
+		'Content-Type': 'application/json'
+	}
+});
 
 export default api;
